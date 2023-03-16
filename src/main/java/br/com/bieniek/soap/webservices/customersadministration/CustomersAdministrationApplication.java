dc771536f3902e5bfb76d568f7f4c245fbff7c3d
Bianeck <thiagobianeck@gmail.com>
2023-03-16 18:02:06 -0300

    Initial commit

---
+++ src/main/java/br/com/bieniek/soap/webservices/customersadministration/CustomersAdministrationApplication.java
@@ -0,0 +1,13 @@
+package br.com.bieniek.soap.webservices.customersadministration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomersAdministrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomersAdministrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bieniek/soap/webservices/customersadministration/CustomersAdministrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bieniek.soap.webservices.customersadministration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomersAdministrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
